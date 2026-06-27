c1dac74293dd358d80b935b8f0b6a0a2a8361db9
NikitosBarbos <nikitabr1337@gmail.com>
2026-06-27 14:09:02 +0600

    feat(task02): implement StudentService with LINQ and tests

---
+++ task02/Student.cs
@@ -0,0 +1,10 @@
+namespace task02
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public List<int> Grades { get; set; }
+    }
+}
+
